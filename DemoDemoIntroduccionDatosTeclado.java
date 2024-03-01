b8543f060b8dc64960ef9b9df90420a7eca7e357
Joelgarsen <joelgsen999@gmail.com>
2024-03-01 13:05:49 +0100

    2

--- DemoDemoIntroduccionDatosTeclado.java
+++ DemoDemoIntroduccionDatosTeclado.java
@@ -14,4 +14,45 @@ public class DemoDemoIntroduccionDatosTeclado {
         
         scanner.close();
     }
+    
+    public void multiplicarDosNumeros() {
+        Scanner scanner = new Scanner (System.in);
+        
+        System.out.print("Ingrese el primer digito: ");
+        int pdigito = scanner.nextInt();
+        
+        System.out.print("Ingrese el segundo digito: ");
+        int sdigito = scanner.nextInt();
+        
+        int resultado = pdigito * sdigito;
+        
+        System.out.print(pdigito + " * " + sdigito + " = " + resultado);
+        
+        scanner.close();
+    }
+    
+    public void convertirDeEuroADolar() {
+        Scanner scanner = new Scanner (System.in);
+        
+        System.out.print("Ingrese euros: ");
+        int euros = scanner.nextInt();
+        
+        double dolar = euros * 1.08;
+        
+        System.out.print("Su valor en dolares es: " + dolar);
+        
+        scanner.close();
+    }
+    
+    public void convertirDeDolarAEuro() {
+        Scanner scanner = new Scanner (System.in);
+        
+        System.out.print("Ingrese dolares: ");
+        int dolar = scanner.nextInt();
+        
+        double euro = dolar * 0.92;
+        
+        System.out.print("Su valor en dolares es: " + euro);
+        scanner.close();
+    }
 }
